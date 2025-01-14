e2a481371c9abddfc3902d464845b88cf436c33c
abdelali <ali18hachadi@gmail.com>
2025-01-14 02:09:53 +0100

    adding project 4

---
+++ projec4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class rectangle 
+{
+    private:
+    int lenght ;
+    int width ;
+    public:
+    rectangle(int Lenght , int Width )
+    {
+        lenght = Lenght ;
+        width = Width ;
+
+    }
+    friend void print(rectangle x , string name);
+    friend void max( rectangle x , rectangle y);
+};
+void print(rectangle x , string name){
+        cout<<"the rect area of rect "<<name<<" is "<<(x.lenght * x.width )<<endl;
+    }
+
+void   max( rectangle x , rectangle y)
+{
+    if ( (x.lenght * x.width ) >= (y.lenght * y.width ))
+    {
+        cout<<"the biggest rect is "<< (x.lenght * x.width )<<endl  ;
+    }
+    else
+    {
+     cout<<"the biggest rect is "<<(y.lenght * y.width )<<endl  ;
+    }
+}
+
+int main ()
+{
+    rectangle first(10 , 19) ;
+    rectangle second(12 , 18) ;
+    print(first , "First");
+    print(second , "Second");
+    max(first , second);
+
+    return 0;
+}
